02f70cfb2eee1ea213e2746d020437a42a232930
Estelaraville <elarav2200@alumno.ipn.mx>
2024-04-10 00:57:44 -0600

    Segundo problema

--- js/problemas.js
+++ js/problemas.js
@@ -21,3 +21,56 @@ function invertir(){
 function borrarDatos(){
     document.getElementById("p1-input").value = "";
 }
+
+function validarNúmeros(event){
+    var tecladonum = (document.all)? event.keyCode : event.which;
+    if(tecladonum == 8) return true;
+    var patron = /[0-9\d .]/;
+    var codigo = String.fromCharCode(tecladonum);
+    return patron.test(codigo);
+}
+
+function vectores(){
+    var x1 = document.getElementById("p2-x1").value;
+    var x2 = document.querySelector("#p2-x2").value;
+    var x3 = document.querySelector("#p2-x3").value;
+    var x4 = document.querySelector("#p2-x4").value;
+    var x5 = document.querySelector("#p2-x5").value;
+
+    var y1 = document.querySelector("#p2-y1").value;
+    var y2 = document.querySelector("#p2-y2").value;
+    var y3 = document.querySelector("#p2-y3").value;
+    var y4 = document.querySelector("#p2-y4").value;
+    var y5 = document.querySelector("#p2-y5").value;
+
+    var vectorx= [x1, x2, x3, x4, x5];
+    var vectory = [y1,y2,y3,y4,y5];
+
+    function ordenar(a, b){
+        return a-b;
+    }
+    
+    vectorx.sort(ordenar);
+    vectory.sort(ordenar);
+    var producto=0;
+    for(var i=0; i<vectorx.length; i++){      
+        producto=producto + (vectorx[i]*vectory[i]);
+    }
+
+    alert("Los números ordenados son: "+vectorx+" y "+vectory+" y el producto escalar mínimo es de : "+producto);
+}
+
+function borrarVectores(){
+    document.getElementById("p2-x1").value = "";
+    document.getElementById("p2-x2").value = "";
+    document.getElementById("p2-x3").value = "";
+    document.getElementById("p2-x4").value = "";
+    document.getElementById("p2-x5").value = "";
+
+    document.getElementById("p2-y1").value = "";
+    document.getElementById("p2-y2").value = "";
+    document.getElementById("p2-y3").value = "";
+    document.getElementById("p2-y4").value = "";
+    document.getElementById("p2-y5").value = "";
+
+}
